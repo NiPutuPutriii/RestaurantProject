8f0b0bf533597769e23fb2b1ebec828d096bac0f
NiPutuPutriii <niputuputri3@gmail.com>
2023-04-15 01:21:19 +0700

    Feature: adding login feature and admin menu

--- src/Resto.java
+++ src/Resto.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Resto {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n--- Online Food Ordering System ---\n");
+            System.out.println("1. Login");
+            System.out.println("2. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = input.nextInt();
+
+            if (choice == 1) {
+                System.out.println("\n--- Login ---\n");
+                System.out.println("1. Admin");
+                System.out.println("2. Customer");
+                System.out.print("Enter your choice: ");
+                int loginChoice = input.nextInt();
+
+                if (loginChoice == 1) {
+                    // Jika login sebagai admin
+                    System.out.println("\n--- Admin Menu ---\n");
+                    System.out.println("1. View Restaurants");
+                    System.out.println("2. Add Restaurant");
+                    System.out.println("3. Delete Restaurant");
+                    System.out.println("4. Logout");
+                    System.out.print("Enter your choice: ");
+                    int adminChoice = input.nextInt();
+                }
+            }
+        }
+    }
 }
