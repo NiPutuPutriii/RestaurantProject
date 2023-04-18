f609168c3e738b0811aa5f966ec81323c75262a3
NiPutuPutriii <113573080+NiPutuPutriii@users.noreply.github.com>
2023-04-19 01:08:45 +0700

    Fixing some bugs

--- src/Login.java
+++ src/Login.java
@@ -1,31 +1,34 @@
 import java.util.Scanner;
-public class Login {
+public class Login{
     static User users = new User();
     static Scanner input = new Scanner(System.in);
     public static boolean Logins() {
-        boolean oke = false;
+        boolean isCheck = false;
         users.setUsername("admin");
         users.setPassword("admin");
 
         users.setUsername_cust("customer");
         users.setPassword_cust("customer");
 
-        String username, password;
-        System.out.print("Masukkan username: ");
+        String username ,password;
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("             > L O G I N   H E R E <             ");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        System.out.print("Username: ");
         username = input.nextLine();
-        System.out.print("Masukkan password: ");
+        System.out.print("Password: ");
         password = input.nextLine();
 
+        //Pengecekan kondisi menggunakan operator logika
         if (users.getUsername().equals(username) || users.getUsername_cust().equals(username)) {
             if (users.getPassword().equals(password)) {
                 users.setStatus("Admin");
-                oke = true;
+                isCheck = true;
             } else if (users.getPassword_cust().equals(password)) {
                 users.setStatus("Customer");
-                oke = true;
+                isCheck = true;
             }
         }
-
-        return oke;
+        return isCheck;
     }
 }
