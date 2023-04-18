571db2540a7c4ca0c63296578a326f1c19fc3215
NiPutuPutriii <113573080+NiPutuPutriii@users.noreply.github.com>
2023-04-19 01:10:01 +0700

    Fix bugs on menu method

--- src/MealMate.java
+++ src/MealMate.java
@@ -1,58 +1,35 @@
-import java.util.Scanner;
-
 public class MealMate {
-    static User users = new User();
-    static Scanner input = new Scanner(System.in);
     static Login signin = new Login();
+    static adminMenu admins = new adminMenu();
 
     public static void menu() {
         int check = 2;
         do {
+            //pengecekan kondisi dari method Logins pada class Login
             if (signin.Logins()) {
                 check = 1;
+                //Pengecekan kondisi apakah true atau false
                 if (signin.users.getStatus().equals("Admin")) {
-                    adminMenu.Admin();
+                    admins.Admin();
                 } else if (signin.users.getStatus().equals("Customer")) {
                     customerMenu.Customer();
                 }
             } else {
-                System.out.println("Username atau Password Salah!");
-                System.out.println("Silakan melakukan login ulang!");
+                System.out.println("                  !!A L E R T!!                  ");
+                System.out.println("          Username atau Password Salah!          ");
+                System.out.println("               Silakan Log In ulang              ");
             }
         } while (check == 2);
     }
+
+    //Fungsi utama yang dimunculkan di awal program
     public static void main(String[] args) {
         System.out.println("*************************************************");
         System.out.println("||               M e a l M a t e               ||");
         System.out.println("||           ..Your Food Companion..           ||");
-        System.out.println("*************************************************\n");
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("             > L O G I N   H E R E <             ");
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        menu();
+        System.out.println("*************************************************");
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        menu(); //Memanggil method menu
     }
 }
-    // Ditampilkan apabila users telah berhasil login
-    /*public static void menu() {
-        // apabila status user admin
-        if (users.getStatus().equals("admin")) {
-            System.out.println("cek12");
-            adminMenu admins = new adminMenu();
-            admins.Admin();
-            // apabila status user customer
-        } else if (users.getStatus() == "Customer") {
-            customerMenu customers = new customerMenu();
-            customers.Customer();
-        }
-    }
 
-    public static void main(String[] args) {
-        if (signin.Logins()) {
-            while (true) {
-                menu();
-            }
-        } else {
-            System.out.println("Username atau Password Salah!");
-        }
-    }
-}*/
